6f482b89aadcb4260fe4d78512801842def374e2
vladys <wawladyslaw@gmail.com>
2024-11-15 15:58:54 +0200

    Initial Commit

---
+++ roll a numTests/roll_a_numTests.swift
@@ -0,0 +1,17 @@
+//
+//  roll_a_numTests.swift
+//  roll a numTests
+//
+//  Created by Vladyslav Tarabunin on 15/11/2024.
+//
+
+import Testing
+@testable import roll_a_num
+
+struct roll_a_numTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
